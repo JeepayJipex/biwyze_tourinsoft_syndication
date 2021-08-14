9a4b7c6dba09573db3bbb33cb83a5408bfe28ac4
Jean Mariette <jmariette@rampar.com>
2021-08-15 00:39:36 +0200

    Initial Commit

---
+++ biwyze_tousinsoft_syndication.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Biwyze Tourinsoft
+Plugin URI: https://biwyze.fr
+Description: Plugin pour l'intégration d'offres présentes sur Tourinsoft à votre site Wordpress
+Version: 1.0
+Author: Jean Mariette
+Author URI: https://biwyze.fr
+License: propriétaire
+*/
+
+require 'vendor/autoload.php';
+
+class BiwyzeTourinsoftSyndication {
+    const VERSION = "1.0.0";
+    const PREFIX = "biwyze_ts";
+    const PLUGIN_NAME = 'Biwyze Tourinsoft Syndication';
+
+    public function __construct() {}
+}
